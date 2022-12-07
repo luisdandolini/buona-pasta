b421fb846e7450b721a103883c2312425f609d22
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-12-07 18:09:52 -0300

    feat: add section information

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
+import { InformationComponent } from './components/information/information.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavComponent
+    NavComponent,
+    InformationComponent
   ],
   imports: [
     BrowserModule,
